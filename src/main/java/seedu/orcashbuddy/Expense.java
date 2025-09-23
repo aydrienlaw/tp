931a63f5343fdcf918d8a8acc3255f6036498151
limzerui <lim.zerui@gmail.com>
2025-09-24 02:13:59 +0800

    feat: add initial orCASHbuddy skeleton (Main, Ui, Expense, ExpenseManager)
    Resolves #5.

---
+++ src/main/java/seedu/orcashbuddy/Expense.java
@@ -0,0 +1,9 @@
+package seedu.orcashbuddy;
+
+/**
+ * Represents an expense entry. Skeleton placeholder for future implementation.
+ */
+public class Expense {
+    // To be implemented in later tasks.
+}
+
---
+++ src/main/java/seedu/orcashbuddy/ExpenseManager.java
@@ -0,0 +1,9 @@
+package seedu.orcashbuddy;
+
+/**
+ * Manages a list of expenses. Skeleton placeholder for future implementation.
+ */
+public class ExpenseManager {
+    // To be implemented in later tasks.
+}
+
