7fa2582829549c656c10a6547acafe5ffb04ed1f
limzerui <lim.zerui@gmail.com>
2025-10-08 15:39:49 +0800

    test(checkstyle): fix EOF newline and LeftCurly in tests

--- src/test/java/seedu/orcashbuddy/ExpenseManagerAddTest.java
+++ src/test/java/seedu/orcashbuddy/ExpenseManagerAddTest.java
@@ -26,8 +26,7 @@ class ExpenseManagerAddTest {
     }
 
     @Test
-    void handleAdd_validAmountAndDescription_addsExpenseAndShowsConfirmation()
-    {
+    void handleAdd_validAmountAndDescription_addsExpenseAndShowsConfirmation() {
         StubUi ui = new StubUi();
         ExpenseManager mgr = new ExpenseManager(ui);
 
