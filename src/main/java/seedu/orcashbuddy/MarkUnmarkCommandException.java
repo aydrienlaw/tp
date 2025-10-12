8646c9433d0c2ff3462448ee5036e63bafab4a85
Muadz Yamani <muadzyamani@gmail.com>
2025-10-12 23:01:50 +0800

    feat: add MarkUnmarkCommandException for handling mark/unmark command errors

---
+++ src/main/java/seedu/orcashbuddy/MarkUnmarkCommandException.java
@@ -0,0 +1,33 @@
+package seedu.orcashbuddy;
+
+/**
+ * Represents an exception thrown when a mark or unmark command cannot be parsed or executed properly.
+ * <p>
+ * This exception is used in {@link seedu.orcashbuddy.ExpenseManager} when:
+ * <ul>
+ *   <li>The mark/unmark command input is missing an index.</li>
+ *   <li>The provided index is not a valid integer.</li>
+ *   <li>The index is less than 1 or exceeds the number of expenses in the list.</li>
+ * </ul>
+ * </p>
+ */
+public class MarkUnmarkCommandException extends Exception {
+    /**
+     * Constructs a new MarkUnmarkCommandException with the specified detail message.
+     *
+     * @param message the detail message describing the error
+     */
+    public MarkUnmarkCommandException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a new MarkUnmarkCommandException with the specified detail message and cause.
+     *
+     * @param message the detail message describing the error
+     * @param cause   the cause of this exception (which can be retrieved later using {@link #getCause()})
+     */
+    public MarkUnmarkCommandException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
