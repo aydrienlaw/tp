98dd090126ec1b3fbb0470c4b2e2a317d2d91e39
aydrienlaw <adrianzelaw@gmail.com>
2025-10-15 02:26:00 +0800

    feat: implement ListCommand for displaying all expenses with budget summary

---
+++ src/main/java/seedu/orcashbuddy/command/ListCommand.java
@@ -0,0 +1,19 @@
+package seedu.orcashbuddy.command;
+
+import seedu.orcashbuddy.storage.ExpenseManager;
+import seedu.orcashbuddy.ui.Ui;
+
+import java.util.logging.Logger;
+
+/**
+ * Command to list all expenses with budget summary.
+ */
+public class ListCommand extends Command {
+    private static final Logger LOGGER = Logger.getLogger(ListCommand.class.getName());
+
+    @Override
+    public void execute(ExpenseManager expenseManager, Ui ui) {
+        LOGGER.fine("Executing list command");
+        expenseManager.displayList(ui);
+    }
+}
