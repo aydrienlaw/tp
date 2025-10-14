8156282b3ef1b4464558b6099352b420d4b23966
aydrienlaw <adrianzelaw@gmail.com>
2025-10-15 01:08:05 +0800

    feat: add abstract Command class for command execution framework

---
+++ src/main/java/seedu/orcashbuddy/command/Command.java
@@ -0,0 +1,13 @@
+package seedu.orcashbuddy.command;
+
+import seedu.orcashbuddy.storage.ExpenseManager;
+import seedu.orcashbuddy.ui.Ui;
+
+public abstract class Command {
+
+    public abstract void execute(ExpenseManager expenseManager, Ui ui) throws Exception;
+
+    public boolean isRunning() {
+        return true;
+    }
+}
