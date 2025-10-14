1497b2e5b8260c74e0d3ede1ec333333d36eafae
aydrienlaw <adrianzelaw@gmail.com>
2025-10-15 02:25:54 +0800

    feat: add InvalidCommand for handling unknown commands

---
+++ src/main/java/seedu/orcashbuddy/command/InvalidCommand.java
@@ -0,0 +1,15 @@
+package seedu.orcashbuddy.command;
+
+import seedu.orcashbuddy.storage.ExpenseManager;
+import seedu.orcashbuddy.ui.Ui;
+
+/**
+ * Command representing an invalid or unknown command.
+ */
+public class InvalidCommand extends Command {
+
+    @Override
+    public void execute(ExpenseManager expenseManager, Ui ui) {
+        ui.showUnknownCommand();
+    }
+}
