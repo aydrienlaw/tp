d3991bd3cf892bd0ca398f04a02abcdb75e1b072
limzerui <lim.zerui@gmail.com>
2025-10-17 19:08:16 +0800

    feat: implement ByeCommand to exit application gracefully

--- src/main/java/seedu/orcashbuddy/command/Command.java
+++ src/main/java/seedu/orcashbuddy/command/Command.java
@@ -5,5 +5,13 @@ import seedu.orcashbuddy.ui.Ui;
 
 public abstract class Command {
     public abstract void execute(ExpenseManager expenseManager, Ui ui) throws Exception;
-}
 
+    /**
+     * Indicates whether executing this command should terminate the application.
+     *
+     * @return true if the application should exit after execution; false otherwise
+     */
+    public boolean isExit() {
+        return false;
+    }
+}
